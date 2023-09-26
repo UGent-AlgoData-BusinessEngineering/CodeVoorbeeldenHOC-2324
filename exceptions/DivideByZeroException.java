dd5d153698a978ee26458ce9a9a2e3a063299b0e
Frederik Gailly <Frederik.Gailly@ugent.be>
2023-09-26 20:55:32 +0200

    Toevoegen code Les 1

--- exceptions/DivideByZeroException.java
+++
@@ -1,9 +0,0 @@
-public class DivideByZeroException extends Exception {
-    public DivideByZeroException() {
-        super("Delen door nul");
-    }
-
-    public DivideByZeroException(String message) {
-        super("Delen door nul: " + message);
-    }
-}
--- exceptions/UnknownOpException.java
+++
@@ -1,9 +0,0 @@
-public class UnknownOpException extends Exception {
-    public UnknownOpException() {
-        super("Operator niet gekend");
-    }
-
-    public UnknownOpException(String message) {
-        super("Operator niet gekend:" + message);
-    }
-}
