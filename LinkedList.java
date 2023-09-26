c89bb23c5dc2026694992bd63bf44ffae85dec56
Frederik Gailly <Frederik.Gailly@ugent.be>
2023-09-26 20:43:37 +0200

    Toevoegen code Les 1

---
+++ LinkedList.java
@@ -0,0 +1,19 @@
+public class LinkedList {
+    LinkedListNode head;
+
+    public LinkedList() {
+        head = null;
+    }
+
+    public void showList(){
+
+    }
+
+    public int length(){
+
+    }
+
+    public void addNodeToStart(String addNodeData){
+
+    }
+}
